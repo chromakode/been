b0324dd93eead3874cca350fc1cb4aa20b8365e6
Jesse Dubay <jesse@thefortytwo.net>
2011-04-13 09:07:23 +0800

    Trim username from github event summaries.

--- been/source/github.py
+++ been/source/github.py
@@ -3,4 +3,9 @@ from been.core import SiteFeedSource, source_registry
 class GitHub(SiteFeedSource):
     url_format = 'http://github.com/{username}.atom'
     kind = 'github'
+    def process_event(self, event):
+        summary = event['summary']
+        if summary.startswith(self.config['username']):
+            event['summary'] = summary[summary.find(' ') + 1:]
+        return event
 source_registry.add(GitHub)
