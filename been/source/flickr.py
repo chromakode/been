0ccf9cec5b6d15202df472ad7237c1bc3632590c
Jesse Dubay <jesse@thefortytwo.net>
2011-04-13 21:56:40 -0700

    Switch Flickr source to the RSS feed, it has thumbnails.

--- been/source/flickr.py
+++ been/source/flickr.py
@@ -1,7 +1,7 @@
 from been.core import SiteFeedSource, source_registry
 
 class Flickr(SiteFeedSource):
-    url_format = 'http://api.flickr.com/services/feeds/photos_public.gne?id={username}&lang=en-us&format=atom'
+    url_format = 'http://api.flickr.com/services/feeds/photos_public.gne?id={username}&lang=en-us&format=rss_200'
     kind = 'flickr'
     def process_event(self, event):
         event['summary'] = 'posted photo ' + event['summary']
