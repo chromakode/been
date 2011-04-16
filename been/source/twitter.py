2694d59b72ee2bcadabed939e6df6dcfee59083f
chromakode <c@chromakode.com>
2011-04-15 23:16:54 -0700

    Clean up twitter source events.

--- been/source/twitter.py
+++ been/source/twitter.py
@@ -4,7 +4,7 @@ class Twitter(SiteFeedSource):
     url_format = 'http://api.twitter.com/1/statuses/user_timeline.atom?screen_name={username}'
     kind = 'twitter'
     def process_event(self, event):
-        status = event['data']['content'][0]['value'].partition(': ')[2]
-        event['summary'] = 'tweeted <q>'+status+'</q>'
+        event['content'] = event['data']['content'][0]['value'].partition(': ')[2]
+        event['summary'] = 'tweeted "'+event['content']+'"'
         return event
 source_registry.add(Twitter)
