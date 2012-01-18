97e8ec7c84e733b7aaf0199e7aea919c42d48842
Max Goodman <c@chromakode.com>
2012-01-17 18:49:34 -0800

    Add configurable keyword filter to the Twitter source.

--- been/sources.py
+++ been/sources.py
@@ -208,10 +208,22 @@ class RedditSource(SiteFeedSource):
 class TwitterSource(SiteFeedSource):
     url_format = 'http://api.twitter.com/1/statuses/user_timeline.atom?screen_name={username}'
     kind = 'twitter'
+
     def process_event(self, event):
         event['content'] = event['data']['content'][0]['value'].partition(': ')[2]
         event['summary'] = 'tweeted "'+event['content']+'"'
-        return event
+        if self.config.get('keyword') and self.config['keyword'].lower() not in event['content'].lower():
+            return None
+        else:
+            return event
+
+    @property
+    def source_id(self):
+        return self.kind+':'+self.config['username']
+
+    @classmethod
+    def configure(cls, username, keyword=None):
+        return cls({'username':username, 'keyword':keyword})
 
 class PublishSource(Source):
     kind = 'publish'
