ff1518730aa1c3c5e991344b30133ece052d70fb
Max Goodman <c@chromakode.com>
2014-07-14 15:59:55 -0700

    Clean up goofy setup.py indentation.

--- setup.py
+++ setup.py
@@ -1,27 +1,27 @@
 from setuptools import setup, find_packages
 
-setup(name="been",
-      description="A life stream collector.",
-      version="0.1",
-      author="Max Goodman",
-      author_email="c@chromakode.com",
-      keywords="feed lifestream",
-      license="BSD",
-      classifiers=[
-          "Programming Language :: Python",
-          "Topic :: Internet :: WWW/HTTP",
-      ],
-      packages=find_packages(),
-      install_requires=[
-            "feedparser",
-            "markdown",
-            "CouchDB",
-            "redis",
-      ],
-      entry_points={
-          "console_scripts": [
-              "been = been.cli:main",
-          ],
-      }
+setup(
+    name="been",
+    description="A life stream collector.",
+    version="0.1",
+    author="Max Goodman",
+    author_email="c@chromakode.com",
+    keywords="feed lifestream",
+    license="BSD",
+    classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Internet :: WWW/HTTP",
+    ],
+    packages=find_packages(),
+    install_requires=[
+        "feedparser",
+        "markdown",
+        "CouchDB",
+        "redis",
+    ],
+    entry_points={
+        "console_scripts": [
+            "been = been.cli:main",
+        ],
+    }
 )
-
